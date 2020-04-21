81ca0a6ad24b02cd153aa0460418191c71d61c8f
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-04-21 22:06:48 +0600

    history 10.06PM
    history...

--- mainpage/history.php
+++ mainpage/history.php
@@ -1,5 +1,6 @@
 <?php
     include "../mainpage/common.inc.php";
+    include "../db/db_connect.inc.php";
     session_start();
     if (!isset($_SESSION['username'])) {
         header("Location: ../login/login.php");
@@ -16,47 +17,33 @@
     <body>
         <div class="table">
             <h1>History Table</h1>
-            <table class="content-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>User ID</th>
-                        <th>Bus ID</th>
-                        <th>Seat No</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>cddf</td>
-                        <td>sdsds</td>
-                        <td>sdsds</td>
-                        <td>dsds</td>
-                        <td>sdsds</td>
-                    </tr>
-                    <tr>
-                        <td>cddf</td>
-                        <td>sdsds</td>
-                        <td>sdsds</td>
-                        <td>dsds</td>
-                        <td>sdsds</td>
-                    </tr>
-                    <tr>
-                        <td>cddf</td>
-                        <td>sdsds</td>
-                        <td>sdsds</td>
-                        <td>dsds</td>
-                        <td>sdsds</td>
-                    </tr>
-                    <tr>
-                        <td>cddf</td>
-                        <td>sdsds</td>
-                        <td>sdsds</td>
-                        <td>dsds</td>
-                        <td>sdsds</td>
-                    </tr>
-                </tbody>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>BUS ID</th>
+                    <th>Seat</th>
+                    <th>Date</th>
+                    <th>Payment</th>
+                    <th>Status</th>
+                </tr>
+                <?php
+                    $sql = "SELECT id,username,bus_id,seat,date,payment,status";
+                    $result = $conn->query($sql);
+                    if($result->num_row > 0){
+                        while($row =$result -> fetch_assoc())
+                        {
+                            echo "<tr><td>".$row["id"]."</td><td>".$row["username"]."</td><td>".$row["bus_id"]."</td><td>".$row["bus_id"]."</td><td>".$row["seat"]."</td><td>".$row["date"]."</td><td>".$row["payment"]."</td><td>".$row[status]."</td><tr>"
+                        }
+                        echo "</table>";
+                    }
+                ?>
             </table>
         </div>
+        <script type="text/javascript">
+            const historyBody= document.querySelector("#history-table>tbody");
+
+            console.log(historyBody);
+        </script>
     </body>
 </html>
