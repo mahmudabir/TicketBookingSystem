5f25037f7b6f2ab3e9380f18ca431e920c3b8eb7
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-04-22 12:16:04 +0600

    history done 12.15 PM
    22 apr

--- mainpage/history.php
+++ mainpage/history.php
@@ -17,16 +17,19 @@
     <body>
         <div class="table">
             <h1>History Table</h1>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>BUS ID</th>
-                    <th>Seat</th>
-                    <th>Date</th>
-                    <th>Payment</th>
-                    <th>Status</th>
-                </tr>
+            <table class="content-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>BUS ID</th>
+                        <th>Seat</th>
+                        <th>Date</th>
+                        <th>Payment</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                
                 <?php
                     $sql = "SELECT * from bus_history";
                     $result = mysqli_query($conn, $sql);
@@ -35,17 +38,15 @@
                     if($rowCount > 0){
                         while($row = mysqli_fetch_assoc($result))
                         {
-                            echo "<tr><td>".$row['id']."</td><td>".$row['username']."</td><td>".$row['bus_id']."</td><td>".$row['seat']."</td><td>".$row['date']."</td><td>".$row['payment']."</td><td>".$row['status']."</td><tr>";
+                            echo "<tbody><tr><td>".$row['id']."</td><td>".$row['username'].
+                            "</td><td>".$row['bus_id']."</td><td>".$row['seat']."</td><td>".
+                            $row['date']."</td><td>".$row['payment']."</td><td>".$row['status'].
+                            "</td></tr></tbody>";
                         }
                         echo "</table>";
                     }
                 ?>
             </table>
         </div>
-        <script type="text/javascript">
-            const historyBody= document.querySelector("#history-table>tbody");
-
-            console.log(historyBody);
-        </script>
     </body>
 </html>
