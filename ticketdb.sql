16bd9064f87b1aa595a45bf46f17cb9cbac7a851
mahmudabir <58890389+mahmudabir@users.noreply.github.com>
2020-04-23 17:27:21 +0600

    sql update

--- ticketdb.sql
+++ ticketdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2020 at 11:41 AM
+-- Generation Time: Apr 23, 2020 at 01:26 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
