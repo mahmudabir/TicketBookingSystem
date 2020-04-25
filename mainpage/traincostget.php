835c8955a6872ff53d86db2569c03993cdd93b1a
Abir Mahmud <amabirmahmud@gmail.com>
2020-04-26 02:23:19 +0600

    Fixed the non_numaric error issue in booking pages
    buscostget.php, traincostget.php, launchcostget.php

--- mainpage/traincostget.php
+++ mainpage/traincostget.php
@@ -1,43 +1,50 @@
 <?php
 
-//include "../mainpage/train.php";
-$mysqli = new mysqli("localhost", "root", "", "ticketdb");
-if ($mysqli->connect_error) {
-    exit('Could not connect');
-}
 
-$sql = "SELECT cost FROM train_list WHERE id = ?";
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if ((!empty($_GET['r'])) && (!empty($_GET['q']))) {
+        //include "../mainpage/bus.php";
+        $mysqli = new mysqli("localhost", "root", "", "ticketdb");
+        if ($mysqli->connect_error) {
+            exit('Could not connect');
+        }
 
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("s", $_GET['q']);
-//$stmt->bind_param("n", $_GET['r']);
-$stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($cost);
-$stmt->fetch();
-$stmt->close();
+        $sql = "SELECT cost FROM bus_list WHERE id = ?";
+        $cost = 0;
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("s", $_GET['q']);
+        //$stmt->bind_param("n", $_GET['r']);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($cost);
+        $stmt->fetch();
+        $stmt->close();
+        $r = $_REQUEST["r"];
 
-function rqst($cost)
-{
-    $r = $_REQUEST["r"];
-    if ($tcost =  $r * $cost) {
-        return $tcost;
+        function rqst($cost, $r)
+        {
+            if (is_numeric($r) && is_numeric($cost)) {
+                $tcost =  $r * $cost;
+                return $tcost;
+              } else {
+                throw new Exception("First Select The requirment fields please.");
+              }
+        }
+
+        try {
+            $tcost = rqst($cost, $r);
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Per Seat Cost: </th>";
+            echo "<td>" . $cost . "_____</td>";
+            echo "<th>Total Cost: </th>";
+            echo "<td>" . $tcost . "</td>";
+            echo "</tr>";
+            echo "</table>";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     } else {
-        //$message = "First Select Seat number & type.";
-        throw new Exception("First Select Seat number & type.");
+        echo "Try again after clicking \"Reset\" button";
     }
-}
-
-try {
-    $tcost = rqst($cost);
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>Per Seat Cost: </th>";
-    echo "<td>" . $cost . "_____</td>";
-    echo "<th>Total Cost: </th>";
-    echo "<td>" . $tcost . "</td>";
-    echo "</tr>";
-    echo "</table>";
-} catch (Exception $e) {
-    echo "Message: " . $e->getMessage();
 }
